48144b3e57d5257de28080fef24252284aa98617
MdRakibHossen0 <130930196+MdRakibHossen917@users.noreply.github.com>
2026-02-19 01:50:44 +0600

    product price and quantity properly increse and decresss

--- src/components/ProductPrice.tsx
+++ src/components/ProductPrice.tsx
@@ -1,7 +1,24 @@
-import { ProductType } from '../../type';
+'use client'
+import { useDispatch, useSelector } from 'react-redux';
+import { ProductType, StateType } from '../../type';
 import PriceFormat from './PriceFormat';
+import { useEffect, useState } from 'react';
 
 const ProductPrice = ({ product }: { product: ProductType }) => {
+
+
+   const { cart } = useSelector((state: StateType) => state.multimart);
+  const dispatch = useDispatch();
+
+  const [existingProduct, setExistingProduct] = useState<ProductType | null>(
+    null,
+  );
+
+  //check product in cart
+  useEffect(() => {
+    const availableProduct = cart?.find((item) => item?.id === product?.id);
+    setExistingProduct(availableProduct || null);
+  }, [cart, product]);
   const discountedPrice =
     product.price - (product.price * product.discountPercentage) / 100;
 
@@ -10,13 +27,13 @@ const ProductPrice = ({ product }: { product: ProductType }) => {
   return (
     <div className="flex items-center gap-2">
       <PriceFormat
-        amount={hasDiscount ? discountedPrice : product.price}
+        amount={existingProduct ? discountedPrice * existingProduct.quantity! : discountedPrice}
         className="text-lg font-bold text-gray-900"
       />
 
       {hasDiscount && (
         <PriceFormat
-          amount={product.price}
+          amount={existingProduct ? product.price * existingProduct.quantity! : product.price}
           className="text-sm text-gray-400 line-through"
         />
       )}
