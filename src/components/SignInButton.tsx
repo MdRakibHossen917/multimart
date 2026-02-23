8200cbf9ba6462fcd0679fea1a9415df93e92f4d
MdRakibHossen0 <130930196+MdRakibHossen917@users.noreply.github.com>
2026-02-24 00:34:21 +0600

    github to login sucessfuly done

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,22 +1,50 @@
-'use client';
-import { signIn } from 'next-auth/react'
-import { LiaUser } from 'react-icons/lia'
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import { LiaUser } from "react-icons/lia";
 
 const SignInButton = () => {
-  return (
-    <div
-    onClick={()=>signIn()}
-      className="flex items-center gap-2 cursor-pointer text-sm"
-    >
-      <div className="border-b border-gray-700 p-1.5 rounded-full text-xl">
-        <LiaUser />
-      </div>
-      <div>
-        <p className="text-xs">Hello, Guests</p>
-        <p className="font-medium">Login/Register</p>
-      </div>
-    </div>
-  )
-}
+  const { data: session, status } = useSession();
 
-export default SignInButton
+  if (status === "loading") return null;
+
+  return (
+    <>
+      {session?.user ? (
+        <div className="flex items-center gap-2 text-sm">
+          <Image
+            src={session?.user?.image || "/default-avatar.png"}
+            alt='User Image'
+            width={40}
+            height={40}
+            className="rounded-full object-cover"
+          />
+          <div>
+            <p className="text-xs">{session.user.name}</p>
+            <button
+              onClick={() => signOut()}
+              className="font-medium hover:underline cursor-pointer text-red-600"
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="flex items-center gap-2 cursor-pointer text-sm"
+        >
+          <div className="border-b border-gray-700 p-1.5 rounded-full text-xl">
+            <LiaUser />
+          </div>
+          <div>
+            <p className="text-xs">Hello, Guest</p>
+            <p className="font-medium">Login / Register</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SignInButton;
